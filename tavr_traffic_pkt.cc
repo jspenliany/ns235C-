d8acb1d51685c4dc32907a6bad965278dfeeba8e
jspenliany <jspenliany@users.noreply.github.com>
2018-03-30 15:26:29 +0800

    Add files via upload

---
+++ tavr_traffic_pkt.cc
@@ -0,0 +1,40 @@
+/*
+ * tavr_traffic_pkt.cc
+ *
+ *  Created on: Mar 16, 2018
+ *      Author: js
+ */
+
+#include <packet.h>
+#include "tavr_traffic_pkt.h"
+
+int hdr_route_tavr::offset_;
+int hdr_veh_hello::offset_;
+int hdr_wired_infos::offset_;
+
+//OTcl linkage for packet headers
+static class TAVRHELLOHeaderClass : public PacketHeaderClass {
+public:
+	TAVRHELLOHeaderClass() : PacketHeaderClass("PacketHeader/TAVRHELLO",
+                                              sizeof(hdr_veh_hello)) {
+	  bind_offset(&hdr_veh_hello::offset_);
+	}
+} class_tavr_hello_hdr;
+
+static class TAVRWIREDHeaderClass : public PacketHeaderClass {
+public:
+	TAVRWIREDHeaderClass() : PacketHeaderClass("PacketHeader/TAVRWIRED",
+                                              sizeof(hdr_wired_infos)) {
+	  bind_offset(&hdr_wired_infos::offset_);
+	}
+} class_tavr_wired_hdr;
+
+static class SIMUTAVRHeaderClass : public PacketHeaderClass {
+public:
+	SIMUTAVRHeaderClass() : PacketHeaderClass("PacketHeader/SIMUTAVR",
+                                              sizeof(hdr_route_tavr)) {
+	  bind_offset(&hdr_route_tavr::offset_);
+	}
+} class_tavr_route_hdr;
+
+
