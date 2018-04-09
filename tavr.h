341e02e72a0f211e1f0078c1361c1b24ac18fefb
jspenliany <jspenliany@users.noreply.github.com>
2018-04-09 14:35:25 +0800

    Add files via upload

--- tavr.h
+++ tavr.h
@@ -8,26 +8,22 @@
 #ifndef TAVR_H_
 #define TAVR_H_
 
-#include "tavr_pkt.h"
+#include "tavr_traffic_pkt.h"
 
 
 #include <agent.h>
 #include <mobilenode.h>
 #include <trace.h>
 #include <classifier.h>
+#include <string>
+#include <classifier-port.h>
+
+using namespace std;
 
 
 
 
 #define CURRENT_TIME		Scheduler::instance().clock();
-#define JITTER				(Random::uniform()*0.5)
-
-#define	VEHICULAR_INFO_PTB_TIMEOUT		10			//delay before send out msg to wired node for future broadcast (6 seconds)
-#define	VEHICULAR_INFO_PTB_RECV_COUNTER	8			//msgs must be received before send to wired node for future broadcast,
-													//including the above, collaboratively decide when to send info to all vehicular
-
-#define TRAFFIC_ROW_AMOUNT				8
-#define	TRAFFIC_COLUMN_AMOUNT			8
 
 
 
@@ -43,33 +39,8 @@ protected:
 	virtual	void expire(Event* e);
 };
 
-class TAVRHello_pktTimer  : public TimerHandler {
-public:
-	TAVRHello_pktTimer(TAVRagent* agent){
-		agent_ = agent;
-	}
-protected:
-	TAVRagent* 	agent_;
-	virtual	void expire(Event* e);
-};
-
-/**
- * for vehicular binding bs-ip
- * */
-class TAVRreplyHello_pktTimer  : public TimerHandler {
-public:
-	TAVRreplyHello_pktTimer(TAVRagent* agent){
-		agent_ = agent;
-	}
-protected:
-	TAVRagent* 	agent_;
-	virtual	void expire(Event* e);
-};
-
 class TAVRagent : public Agent{
 
-	friend class TAVRHello_pktTimer;
-	friend class TAVRreplyHello_pktTimer;
 	friend class TAVR_pktTimer;
 
 private:
@@ -88,13 +59,14 @@ private:
 	double		firstHellorecv_ts_;
 
 	//msg timer
-	TAVRHello_pktTimer 			hello_timer_;
-	TAVRreplyHello_pktTimer		reply_hello_timer_;
+	TAVR_pktTimer 	tavr_timer_;
+	u_int32_t	inner_seqno_;
 
 
 
 
 	//vehicular info
+	double 		vehicle_info_updateTime_LIST_[VEHICULAR_AMOUNT]; //
 	nsaddr_t 	vehicle_ip_LIST_[VEHICULAR_AMOUNT]; //device ID----vehicle
 //	nsaddr_t 	all_ip_; //device ID----vehicle
 	double 		vehicle_position_x_LIST_[VEHICULAR_AMOUNT]; //
@@ -103,13 +75,10 @@ private:
 	u_int8_t 	vehicle_direction_LIST_[VEHICULAR_AMOUNT]; //
 	bool 		junction_LIST_[VEHICULAR_AMOUNT];
 	nsaddr_t 	bs_ip_LIST_[VEHICULAR_AMOUNT];
-
-	//traffic calculation
-	u_int16_t		map_traffic[TRAFFIC_ROW_AMOUNT+1][TRAFFIC_COLUMN_AMOUNT+1];//for road segments
-	cjunction_info	map_junc_list[TRAFFIC_ROW_AMOUNT*TRAFFIC_COLUMN_AMOUNT];// for junctions gps info
-	double			map_X;
-	double			map_Y;
-
+	u_int8_t 	junc_row_prev_list[VEHICULAR_AMOUNT]; //
+	u_int8_t 	junc_col_prev_list[VEHICULAR_AMOUNT]; //
+	u_int8_t 	junc_row_next_list[VEHICULAR_AMOUNT]; //
+	u_int8_t 	junc_col_next_list[VEHICULAR_AMOUNT]; //
 
 
 
@@ -117,7 +86,7 @@ private:
 	//
 	MobileNode *node_;
 	Trace *logtarget_;
-	NsObject *port_dmux_;
+	PortClassifier *dmux_;
 
 
 	//otcl command methods for configuration parameter
@@ -129,13 +98,28 @@ private:
 	int			conf_scenario_vehicular_amout;
 	int			conf_scenario_base_amout;
 	int			conf_node_id;
+	int			conf_junc_row_bs;
+	int			conf_junc_col_bs;
+
+	double		conf_startx_;
+	double		conf_starty_;
+	int			conf_base_rowc;
+	int			conf_base_colc;
+	double		**junc_info_arrayX;
+	double		**junc_info_arrayY;
+	int			comm_id;
+
 
 	int			conf_test_INET;//bits VALID: six bits, first FLAG, second BROADCAST, third SUBNET, last INTERNET
 
-	int         conf_debug;
+	int         conf_test_AXIS_ip;
+
+
+	bool		debug_flag;
 
 
 protected:
+
 	inline nsaddr_t& vehicle_ip() {
 		return vehicle_ip_;
 	}
@@ -154,11 +138,14 @@ protected:
 	inline bool& junction() {
 		return junction_;
 	}
-	inline nsaddr_t& bs_ip() {
+	inline nsaddr_t bs_ip() {
 		return bs_ip_;
 	}
 
 
+	inline double* vehicle_info_updateTime_list() {
+		return vehicle_info_updateTime_LIST_;
+	}
 	inline nsaddr_t* vehicle_ip_LIST() {
 		return vehicle_ip_LIST_;
 	}
@@ -183,6 +170,20 @@ protected:
 	inline u_int8_t& hello_amount() {
 		return hello_amount_;
 	}
+	inline u_int8_t* junc_row_prev_List() {
+		return junc_row_prev_list;
+	}
+	inline u_int8_t* junc_col_prev_List() {
+		return junc_col_prev_list;
+	}
+	inline u_int8_t* junc_row_next_List() {
+		return junc_row_next_list;
+	}
+	inline u_int8_t* junc_col_next_List() {
+		return junc_col_next_list;
+	}
+
+
 	inline double& firstHellorecv_ts() {
 		return firstHellorecv_ts_;
 	}
@@ -190,42 +191,32 @@ protected:
 		double tmp = CURRENT_TIME;
 		return ((firstHellorecv_ts_ + delay) < tmp);
 	}
+
+
+
+
+
 	void update_vehicular_info();
-
-
-
-
-public:
-	double* 	cvehicle_position_x_LIST();
-	double* 	cvehicle_position_y_LIST();
-	double* 	cvehicle_speed_LIST();
-	u_int8_t* 	cvehicle_direction_LIST();
-	bool* 		cjunction_LIST();
-	nsaddr_t* 	cbs_ip_LIST();
+	bool timeValid_compare(double,double);
 
 public:
 	TAVRagent(nsaddr_t);
+	~TAVRagent();
 	int command(int argc, const char*const* argv);
 	void recv(Packet*, Handler* callback = 0);
-	void sendHello();
-	void reset_Hello_timer();
-	void replyHello();
-	void reset_replyHello_timer();
-	void recv_TAVR(Packet*);
-	void forward_data(Packet*);
-	void recv_Hello(Packet*);
+	void send_tavr_pkt();
+	void reset_tavr_timer();
+	void tavr_forward_data(Packet*);
+	void recv_tavr_pkt(Packet*);
 	void recv_Wired(Packet*);
-	void recv_Rabbitall();
-
+	void recv_Hello(Packet*);
+	void init_juncInfo();
+	void print_bs();
 
 	void base_sendBeacon();
 
 
 //---------------test part ----------------------
-	void print_bs();
-protected:
-	void print_addr(nsaddr_t);
-	int  get_indexFromaddr(nsaddr_t);
 
 
 };
